facc38db6f115e8830854ab71a63fcb9894f5c35
Kernkraftwerk <kernkraftdev@hotmail.com>
2025-12-30 21:20:05 +0100

    Add files via upload

--- config.h
+++ config.h
@@ -5,39 +5,39 @@
 #define TERMINALCLASS "St"
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 10;				/* gaps between windows */
+static const unsigned int gappx     = 15;				/* gaps between windows */
 //rstolen
-static unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static unsigned int gappih    = 40;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 40;       /* vert inner gap between windows */
+static unsigned int gappoh    = 60;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 60;       /* vert outer gap between windows and screen edge */
 //
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static 			 int smartgaps				  = 0;
+static const unsigned int systrayspacing = 5;   /* systray spacing */
+static 			 int smartgaps				  = 1;
 static const int swallowfloating    = 1;
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "terminus:size=16.5" };
-static const char dmenufont[]       = "terminus:size=17";
-static char normbgcolor[]           = "#000000";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
-static char selbgcolor[]            = "#005577";
+static const char *fonts[]          = { "monospace:size=12"};
+static const char dmenufont[]       = "terminus:size=12";
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#333333";
+static char normfgcolor[]           = "#888888";
+static char selfgcolor[]            = "#FFFFFF";
+static char selbordercolor[]        = "#4C7899";
+static char selbgcolor[]            = "#285577";
 static char *colors[][3] = {
 /*               fg           bg           border   */
 [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-[SchemeSel]  = { selfgcolor, selbgcolor ,  selbgcolor},
-};		      ;
+[SchemeSel]  = { selfgcolor, selbgcolor ,  selbordercolor},
+};
 
 /* tagging */
-static const char *tags[] = { "www", "dev", "dev-term", "term", "dox", "mux", "mail", "virt", "pass" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 /* xprop(1):
@@ -46,7 +46,7 @@ static const Rule rules[] = {
  */
 /* class      instance    title       tags mask     isfloating   monitor */
 { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
